4b6746f805c3d21b5a627dae13a3d35b4061b0e3
Maria Nita <maria.nita.dn@gmail.com>
2016-04-28 13:06:44 +0300

    Use psutil to count process open files

--- graphite/mylocalmcds/open_files.py
+++ graphite/mylocalmcds/open_files.py
@@ -1,9 +1,10 @@
 import os
 import socket
-import subprocess
 from time import gmtime, strftime
 
 from pykstats.common.base_statsd_plotter import BaseStatsdPlotter
+
+import psutil
 
 
 class MylocalmcdsOpenFilesPlotter(BaseStatsdPlotter):
@@ -21,13 +22,7 @@ class MylocalmcdsOpenFilesPlotter(BaseStatsdPlotter):
         with open(config['neo4j']['pid_file_path'], 'r') as neo4j_pid_file:
             neo4j_pid = neo4j_pid_file.read().strip('\n')
 
-        # Compute metric value
-        command1 = subprocess.Popen(["lsof", "-p %d" % neo4j_pid],
-            stdout=subprocess.PIPE)
-        command2 = subprocess.Popen(["wc", "-l"], stdin=command1.stdout,
-            stdout=subprocess.PIPE)
-        command1.stdout.close()
-        value, error = command2.communicate()
+        value = psutil.Process(neo4j_pid).get_open_files()
 
         # Send data to statsd daemon.
         self.statsd.gauge(self.metric, value)
